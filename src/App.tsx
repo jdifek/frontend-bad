a776748be6fcd638c3ceabf50c906fdfbd953f3a
jdifek <givotkov11@gmail.com>
2025-04-18 22:55:22 +0300

    wqwq

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,8 @@ export const App = () => {
     const tg = window.Telegram.WebApp;
     if (tg) {
       tg.ready();
-      // Проверяем поддержку Bot API 8.0+
-      if (tg.isVersionAtLeast('8.0')) {
-        // Запрашиваем полноэкранный режим
-        tg.requestFullscreen();
-        // Устанавливаем цвет заголовка (рекомендуется в полноэкранном режиме)
-        tg.setHeaderColor('#000000'); // Установите подходящий цвет, например, чёрный
-        // Проверяем, активирован ли полноэкранный режим
-        console.log('Полноэкранный режим активен:', tg.isFullscreen);
-      } else {
-        // Для старых версий используем expand()
-        tg.expand();
-      }
+      tg.requestFullscreen();
+      
     }
   }, []);
 
