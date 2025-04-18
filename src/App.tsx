e687789eb32050702209805e10c7cc90b87a0a3a
jdifek <givotkov11@gmail.com>
2025-04-18 22:41:06 +0300

    wqwq

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import { ToastContainer } from 'react-toastify'
 export const App = () => {
   useEffect(() => {
     const tg = window.Telegram.WebApp;
-		if (tg) {
-			tg.ready()
-      tg.requestFullscreen();
-    		}
-	}, [])
+    if (tg) {
+      tg.ready();
+      tg.expand(); // Можно оставить для совместимости со старыми версиями
+      tg.requestFullscreen(); // Запрашиваем полноэкранный режим
+      // Рекомендуется установить цвет заголовка
+    }
+  }, []);
 
-	return (
-		<div className='min-h-screen bg-neutral-beige'>
-			<Navigation />
-			<div className='max-w-5xl mx-auto p-4'>
-				<RoutesConfig />
-			</div>
-			<ToastContainer />
-		</div>
-	)
+  return (
+    <div className='min-h-screen bg-neutral-beige'>
+      <Navigation />
+      <div className='max-w-5xl mx-auto p-4'>
+        <RoutesConfig />
+      </div>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default App
+export default App;
