b10a29ccc6f00233a1f92d6d3b0f01d28dba0be4
jdifek <givotkov11@gmail.com>
2025-06-03 12:12:38 +0300

    wqwqwq

--- src/pages/FoodAnalysis.tsx
+++ src/pages/FoodAnalysis.tsx
@@ -596,7 +596,7 @@ export const FoodAnalysis = () => {
 {/* Модальное окно для ввода потраченных калорий */}
 {showBurnedModal && (
   <motion.div
-    className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4"
+    className="fixed inset-0 bg-blue-100 bg-opacity-50 flex items-center justify-center p-4"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
   >
